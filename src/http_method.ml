963406a42e280580b279aad8feb4944a9af41a71
Martin Jambon <martin@mjambon.com>
2014-07-09 17:42:27 -0700

    Added OPTIONS and PATCH methods.

--- src/http_method.ml
+++ src/http_method.ml
@@ -1,10 +1,12 @@
 (** HTTP request method *)
-type t = [`DELETE | `GET | `HEAD | `POST | `PUT]
+type t = [ `DELETE | `GET | `HEAD | `OPTIONS | `PATCH | `POST | `PUT ]
 
 let of_string = function
   | "DELETE" -> `DELETE
   | "GET"    -> `GET
   | "HEAD"   -> `HEAD
+  | "OPTIONS"-> `OPTIONS
+  | "PATCH"  -> `PATCH
   | "POST"   -> `POST
   | "PUT"    -> `PUT
   | s        -> failwith ("Invalid request method: " ^ s)
@@ -13,5 +15,7 @@ let to_string = function
   | `DELETE  -> "DELETE"
   | `GET     -> "GET"
   | `HEAD    -> "HEAD"
+  | `OPTIONS -> "OPTIONS"
+  | `PATCH   -> "PATCH"
   | `POST    -> "POST"
   | `PUT     -> "PUT"
--- src/http_method.mli
+++ src/http_method.mli
@@ -1,5 +1,5 @@
 (** HTTP request method *)
-type t = [ `DELETE | `GET | `HEAD | `POST | `PUT ]
+type t = [ `DELETE | `GET | `HEAD | `OPTIONS | `PATCH | `POST | `PUT ]
 
 val of_string : string -> t
 val to_string : t -> string
