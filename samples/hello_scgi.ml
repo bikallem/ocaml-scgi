0b999119b1dee24f564eb0b322b98ceab96328ab
Bikal Gurung <gbikal@gmail.com>
2019-04-06 16:09:47 +0100

    ocamlformat samples

--- samples/hello_scgi.ml
+++ samples/hello_scgi.ml
@@ -14,7 +14,8 @@ let () =
          , Printf.sprintf " port number (default: %d)" !port )
        ; ( "--addr"
          , Arg.Set_string addr
-         , Printf.sprintf " ip address to bind (default: %s)" !addr ) ])
+         , Printf.sprintf " ip address to bind (default: %s)" !addr )
+       ])
     (fun s -> failwith (Printf.sprintf "Unknown argument: [%s]" s))
     "try --help" ;
   (* Start the handler *)
@@ -29,8 +30,9 @@ let () =
         in
         Lwt.return
           { Response.status= `Ok
-          ; headers= [`Content_type "text/plain"]
-          ; body= `String body } )
+          ; headers= [ `Content_type "text/plain" ]
+          ; body= `String body
+          } )
   in
   (* Run forever in foreground. *)
   Lwt_main.run (fst (Lwt.wait ()))
